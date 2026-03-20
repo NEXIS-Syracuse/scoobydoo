c61dee17070a6d7ef4395dfb3633986be84e3837
Lauren Gracias <lgracias2022@gmail.com>
2026-03-20 15:01:46 -0400

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ charizard = st.Page("Pokemon_Characters/charizard.py", title="HW 1", icon="📄"
 eevee = st.Page("Pokemon_Characters/eevee.py", title="HW 2 - URL Summarizer", icon="🌐")
 mewtwo = st.Page("Pokemon_Characters/mewtwo.py", title="HW 3 - URL Chatbot", icon=":material/description:")
 lucario = st.Page("Pokemon_Characters/lucario.py", title ="HW 4 - iSchool Org Chatbot", icon=":material/description:")
-clint = st.Page("Stardew_Characters/clint.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:", default=True)
-maru = st.Page("Stardew_Characters/maru.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:", default=True)
-haley = st.Page("Stardew_Characters/haley.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:", default=True)
+clint = st.Page("Stardew_Characters/clint.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:")
+maru = st.Page("Stardew_Characters/maru.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:")
+haley = st.Page("Stardew_Characters/haley.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:")
 
 
 
