24525b8b44036de48c77ddcd1c4295b9a2f1d42b
Lauren Gracias <lgracias2022@gmail.com>
2026-03-22 13:28:41 -0400

    npc_home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 intro = st.Page("intro.py", title="NPC Playground", icon=":/material/description:", default= True)
-charizard = st.Page("Pokemon_Characters/charizard.py", title="Charizard", icon="")
-eevee = st.Page("Pokemon_Characters/eevee.py", title="Eevee", icon="🌐")
-mewtwo = st.Page("Pokemon_Characters/mewtwo.py", title="Mewtwo", icon=":material/description:")
-lucario = st.Page("Pokemon_Characters/lucario.py", title ="Lucario", icon=":material/description:")
-clint = st.Page("Stardew_Characters/clint.py", title ="", icon=":material/description:")
-maru = st.Page("Stardew_Characters/maru.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:")
-haley = st.Page("Stardew_Characters/haley.py", title ="HW 5 - SU Org Memory Chatbot", icon=":material/description:")
+charizard = st.Page("Pokemon_Characters/charizard.py", title="Charizard", icon="🔥")
+eevee = st.Page("Pokemon_Characters/eevee.py", title="Eevee", icon="🐕")
+mewtwo = st.Page("Pokemon_Characters/mewtwo.py", title="Mewtwo", icon="🔮")
+lucario = st.Page("Pokemon_Characters/lucario.py", title ="Lucario", icon="🔵")
+clint = st.Page("Stardew_Characters/clint.py", title ="Clint", icon="⚒️")
+maru = st.Page("Stardew_Characters/maru.py", title ="Maru", icon="🩺")
+haley = st.Page("Stardew_Characters/haley.py", title ="Haley", icon="📸")
 
-pg = st.navigation([charizard, eevee, mewtwo, lucario, clint, maru, haley])
+pg = st.navigation([intro, charizard, eevee, mewtwo, lucario, clint, maru, haley])
 
 st.set_page_config(page_title="NPC Playground", page_icon="🎭")
 
