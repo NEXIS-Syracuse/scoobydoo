0b21db961a929b4b00abe2ed32a84f596c585dd9
Lauren Gracias <lgracias2022@gmail.com>
2026-03-25 09:41:38 -0400

    update_home

---
+++ intro.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Page config
+st.set_page_config(page_title="NPC Dialogue", page_icon="🎭", layout="centered")
+
+st.title("🎭 NPC Dialogue System")
+st.caption("Chat with fantasy characters from the world of Pokemon and Stardew Valley powered by OpenAI")
